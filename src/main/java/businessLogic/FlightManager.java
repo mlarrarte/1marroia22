5e7bf6f6f65f5445d86dd9f8dcb8ec2a00eaf1ee
joniturrioz <joniturrioz@172.20.10.2>
2022-12-16 19:22:48 +0100

    2023 updated

--- src/main/java/businessLogic/FlightManager.java
+++ src/main/java/businessLogic/FlightManager.java
@@ -3,7 +3,15 @@ package businessLogic;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.Vector;
 
 import domain.ConcreteFlight;
 import domain.Flight;
@@ -16,23 +24,41 @@ ArrayList<Flight> flightsDB;
 			
 			Flight f1 = new Flight("F1","Donostia","Bilbo");
 			
-			f1.addConcreteFlight("CF1",newDate(2022,1,22),1,2,3,"12:00");
-			f1.addConcreteFlight("CF2",newDate(2022,1,23),3,0,3,"12:00");
-			f1.addConcreteFlight("CF3",newDate(2022,1,23),1,2,2,"13:00");
-			f1.addConcreteFlight("CF4",newDate(2022,1,23),3,3,0,"14:00");
-			f1.addConcreteFlight("CF5",newDate(2022,1,23),3,3,0,"15:00");
-			f1.addConcreteFlight("CF6",newDate(2022,1,23),3,3,0,"16:00");
-			f1.addConcreteFlight("CF7",newDate(2022,1,23),3,3,0,"17:00");
-																								
+			f1.addConcreteFlight("CF1-1",newDate(2023,1,22),1,2,3,"12:00");
+			f1.addConcreteFlight("CF1-2",newDate(2023,1,23),3,0,3,"12:00");
+			f1.addConcreteFlight("CF1-3",newDate(2023,1,23),1,2,2,"13:00");
+			f1.addConcreteFlight("CF1-4",newDate(2023,1,23),3,3,0,"14:00");
+			f1.addConcreteFlight("CF1-5",newDate(2023,1,23),3,3,0,"15:00");
+			f1.addConcreteFlight("CF1-6",newDate(2023,1,23),3,3,0,"16:00");
+			f1.addConcreteFlight("CF1-7",newDate(2023,1,23),3,3,0,"17:00");
+					
+			Flight f2 = new Flight("F2","Donostia","Madrid");
 			
-			Flight f2 = new Flight("F2","Bilbo","Donostia");
-			f2.addConcreteFlight("CF8",newDate(2022,1,21),3,3,0,"12:00");
-			f2.addConcreteFlight("CF9",newDate(2022,1,22),3,3,0,"12:00");
-			f2.addConcreteFlight("CF10",newDate(2022,1,23),3,3,0,"12:00");
+			Flight f3 = new Flight("F3","Barcelona","Madrid");
+			Flight f4 = new Flight("F4","Sevilla","Madrid");
+			Flight f5 = new Flight("F5","Barcelona","Madrid");
 
 			
-			flightsDB.add(f1);	
-			flightsDB.add(f2);		
+
+
+			
+			Flight f21 = new Flight("F21","Bilbo","Donostia");
+			f21.addConcreteFlight("CF21-1",newDate(2023,1,21),3,3,0,"12:00");
+			f21.addConcreteFlight("CF21-2",newDate(2023,1,22),3,3,0,"12:00");
+			f21.addConcreteFlight("CF21-3",newDate(2023,1,23),3,3,0,"12:00");
+
+
+			flightsDB.add(f1);
+			flightsDB.add(f2);
+			flightsDB.add(f3);
+			flightsDB.add(f4);
+			flightsDB.add(f5);
+			
+
+			flightsDB.add(f21);	
+			
+			System.out.println(getAllDepartingCities());
+			System.out.println(getArrivalCitiesFrom("Donostia"));
 			
 		}
 		public Collection<ConcreteFlight> getConcreteFlights(String departingCity, String arrivingCity, Date date) {
@@ -46,6 +72,33 @@ ArrayList<Flight> flightsDB;
 				return res;
 				
 		}
+		
+		public List<String> getAllDepartingCities(){
+
+			if (flightsDB.isEmpty()) return new ArrayList<String>();
+			//A set is used to avoid duplicates
+			SortedSet<String> setDepartingCities = new TreeSet<String>();
+			Iterator<Flight> i=flightsDB.iterator();
+			while (i.hasNext())
+				setDepartingCities.add(i.next().getDepartingCity());
+			return new ArrayList<String>(setDepartingCities);			
+		}
+		
+		public List<String> getArrivalCitiesFrom(String departingCity){
+
+			List<String> arrivalCities = new ArrayList<String>();
+
+			Iterator<Flight> i=flightsDB.iterator();
+			Flight f;
+			while (i.hasNext()) {
+				f=i.next();
+				if (f.getDepartingCity().compareTo(departingCity)==0)
+					arrivalCities.add(f.getArrivingCity());
+			}
+			Collections.sort(arrivalCities);
+			return arrivalCities;
+			
+		}
 
 		private Date newDate(int year,int month,int day) {
 
@@ -55,6 +108,8 @@ ArrayList<Flight> flightsDB;
 
 		     return calendar.getTime();
 		}
+		
+		
 		
 		
 }
--- src/main/java/businessLogic/FlightManagerInterface.java
+++ src/main/java/businessLogic/FlightManagerInterface.java
@@ -2,9 +2,12 @@ package businessLogic;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import domain.ConcreteFlight;
 
 public interface FlightManagerInterface{
 	Collection<ConcreteFlight> getConcreteFlights(String departingCity, String arrivingCity, Date date);
+	public List<String> getAllDepartingCities();
+	public List<String> getArrivalCitiesFrom(String departingCity);
 }
